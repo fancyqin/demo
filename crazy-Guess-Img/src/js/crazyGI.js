41402df352f99d5ad175b143377c5d7b3a5113d4
Fan <fancarter007@163.com>
2016-04-14 20:05:16 +0800

    change

---
+++ crazy-Guess-Img/src/js/crazyGI.js
@@ -0,0 +1,10 @@
+void function(){
+    var datas = {
+        imgSrc: 'http://design.vemic.com:2222/girl/300x300'
+    };
+    var vm =new Vue({
+        el:'#app',
+        data:datas
+    });
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+}();
