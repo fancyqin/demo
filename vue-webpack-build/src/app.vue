3185903728a15cdeca8370b8ae7661941e073db3
Fan <fancarter007@163.com>
2016-04-22 15:56:39 +0800

    update

---
+++ vue-webpack-build/src/app.vue
@@ -0,0 +1,24 @@
+<style lang="sass" scoped>
+    .app{
+        h2{
+            color: red;
+        }
+    }
+</style>
+
+<template>
+    <div class="app">
+        <h2>{{msg}}</h2>
+        <div>aa</div>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data: function(){
+            return{
+                msg:'qwerty ddd'
+            }
+        }
+    }
+</script>
