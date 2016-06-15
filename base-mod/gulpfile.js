ad42b358143757df9f0cce76e0dec91ab4784e6b
Fan <fancarter007@163.com>
2016-06-15 17:45:33 +0800

    new base-mod

---
+++ base-mod/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+    return sass('src/css/**/*.scss',{sourcemap:true})
+        .on('error', sass.logError)
+        // For inline sourcemaps
+        .pipe(sourcemaps.write())
+
+        // For file sourcemaps
+        .pipe(sourcemaps.write('maps', {
+            includeContent: false,
+            sourceRoot: 'source'
+        }))
+        .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/css/**/*css', ['sass']);
+});
