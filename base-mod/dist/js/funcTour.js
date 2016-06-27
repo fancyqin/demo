191df7395444a6c627e2062052f30858f6db4f8a
Fan <fancarter007@163.com>
2016-06-27 11:20:02 +0800

    funcTour

--- base-mod/dist/js/funcTour.js
+++ base-mod/dist/js/funcTour.js
@@ -2,8 +2,8 @@
 
     var defaults = {
         item:[
-            ['.J-step-1','center bottom',"hello, I'm the first steps"],
-            ['.J-step-2','center bottom',"hello, I'm the second steps"]
+            ['.J-step-1',"hello, I'm the first steps",'center bottom'],
+            ['.J-step-2',"hello, I'm the second steps",'center bottom']
         ],
         zindex: 100000
     };
@@ -24,37 +24,66 @@
         (stepNum < 1) && console.error('err');
 
 
-        stepNum = 1;//////////////////
-
-
         for (var i = 0;i<stepNum;i++){
             var item =  items[i];
-            (item.length<2) && console.error('err');
-            
-            Tour(item,zindex);
+            (item.length<1) && console.error('err');
+
         }
+        _this.tourTo(0);
 
         $tour.on('click','.ft-close',function (e) {
             e.stopPropagation();
             e.preventDefault();
             _this.closeTour();
+        }).on('click','.ft-next',function(e){
+            var lastNum = _this.lastNum;
+            e.stopPropagation();
+            e.preventDefault();
+            _this.tourTo(lastNum + 1);
         })
 
     };
 
-    function Tour(item,zindex){
+    FuncTour.prototype.lastNum = 0;
 
+    FuncTour.prototype.beginTour = function () {
+        this.tourTo(0);
+        $tour.show();
+    };
+
+    FuncTour.prototype.closeTour = function () {
+        console.log(this.conf);
+        $tour.hide();
+    };
+
+    FuncTour.prototype.tourTo = function (num) {
+        var lastNum = this.lastNum;
+
+        var items = this.conf.item;
+        var item = items[num];
         var $el = $(item[0]);
         var el = $el[0];
-        var direction = item[1] || 'center bottom';
-        var txt = item[2] || 'please set some words';
-        var el_pos =  (window.getComputedStyle && window.getComputedStyle(el).position) || el.currentStyle.position || el.style.position;
+        var zindex = this.conf.zindex;
+        var txt = item[1] || 'please set some words';
+        var direction = item[2] || 'center bottom';
+        //var el_pos =  (window.getComputedStyle && window.getComputedStyle(el)['position']) || el.currentStyle['position'] || el.style['position'];
+        var el_pos = getCssStyle(el,'position');
+        var el_zindex = getCssStyle(el,'z-index');
         var elInfo = {
             width :  el.clientWidth,
             height : el.clientHeight,
             top :el.offsetTop,
             left : el.offsetLeft
         };
+
+        //clear lastNum styles
+        var $lastEl = $(items[lastNum][0]);
+        $lastEl[0].style.zIndex = 0;
+        console.log($lastEl[0].style);
+
+
+
+
         if (el_pos ==='static'){
             $el.css({
                 'position':'relative',
@@ -72,21 +101,14 @@
             left: elInfo.left
         });
         $txtBox.text(txt);
-
-
-    }
-
-    FuncTour.prototype.beginTour = function (cb) {
-        $tour.show();
-        cb();
+        this.lastNum = num;
     };
 
-    FuncTour.prototype.closeTour = function () {
-        $tour.hide();
-    };
 
-    FuncTour.prototype.tourTo = function (num) {
-        //todo
+    function getCssStyle(el,attr){
+        var attr = attr.toString();
+        var value = (window.getComputedStyle && window.getComputedStyle(el)[attr]) || el.currentStyle[attr] || el.style[attr];
+        return value;
     }
 
     window.FuncTour = FuncTour;
