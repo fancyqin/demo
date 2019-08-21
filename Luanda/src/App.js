89639de5f9e03dba4e734e105191b990ca66a61e
秦帆 <qinfan@qinfandeMac-mini.local>
2019-08-21 10:01:34 +0800

    fix

--- Luanda/src/App.js
+++ Luanda/src/App.js
@@ -5,7 +5,7 @@ import React,{Component} from 'react'
 export default class App extends Component {
     render(){
         return <div>
-            <img src="../src/assets/img/gewgw/Simulato.png" />
+            <img src={require('./assets/img/gewgw/Simulato.png')} width="100" />
             hello luanda</div>
         
     }
